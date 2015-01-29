a7b1f6334049c40a4b169d7d035d7220ab668356
christianhv <christian.hv@me.com>
2015-01-29 11:14:41 +0100

    segundo con más funciones

--- segundo.go
+++ segundo.go
@@ -6,20 +6,25 @@ import "fmt"
 
 func main(){
 	var x string = "Hello, World"
-	var b bool = false
+	var b bool
 	fmt.Println(x)
 	boo()
 	b = tonto()
-	fmt.Println(b)
+	fmt.Println("The value received by tonto is:", b)
+	cadenas("uno", "dos")
 }
 
 func boo(){
 	fmt.Println(true && true)
 }
 
-func tonto(){
+func tonto()(bool){
 	var y bool
 	y = true && true
-	
 	return y
+}
+
+func cadenas(x string, y string){
+	var una, dos string =  x, y
+	fmt.Println("La primera es: " + una + ", la segunda es: " + dos)
 }
